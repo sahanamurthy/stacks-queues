45715f20495977ed265301a57518b7bdba17d64c
Sahana Murthy <s.murthy730.com>
2017-02-23 12:49:42 -0800

    Added my stacks and queue programs

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,33 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    return @store.push(element)
   end
 
   def dequeue
+    return @store.shift
   end
 
   def front
+    return @store.first
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    if @store.length == 0
+      return true
+    else
+      return false
+    end
   end
 
   def to_s
+    return @store.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,28 @@ class Stack
   end
 
   def push(element)
+    # @store.unshift(element)  #Either of these two methods work
+    @store.push(element)
   end
 
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    if @store.length == 0
+      return true
+    else
+      return false
+    end
   end
 
   def to_s
